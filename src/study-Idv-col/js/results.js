a5027f11d392aa3375cbc4ff34a91fe9ea2e53ef
drishya-nair <nair.drish@gmail.com>
2024-05-21 12:47:59 -0700

    fixed bar graph offset issue

--- src/study-Idv-col/js/results.js
+++ src/study-Idv-col/js/results.js
@@ -3,10 +3,10 @@
     const MAX_GRAPH_WIDTH = 900;
     const MAX_GRAPH_HEIGHT = 300;
     const MAX_SCORE = 30;
+    const offset = 3;
 
-    // Declare the chart dimensions and margins.
     const width = Math.min(PAGE_CONTENT_WIDTH, MAX_GRAPH_WIDTH);
-    const svgWidth = width + 25;
+    const svgWidth = width + 45;
     const height = MAX_GRAPH_HEIGHT;
     const barHeight = height / 7.5;
     const halfWidth = width / 2;
@@ -15,6 +15,9 @@
     const colors2 = ['rgb(253,212,158)', 'rgb(239,101,72)'];
 
     const _calculateMarkX = function (score) {
+      if (score > MAX_SCORE - offset) {
+        return (width / MAX_SCORE) * score - offset;
+      }
       return (width / MAX_SCORE) * score;
     };
 
@@ -45,13 +48,11 @@
     };
 
     const draw = function (divID) {
-      // Create the SVG container.
       svg = d3.select(`#${divID}`)
         .append("svg")
         .attr("width", svgWidth)
         .attr("height", height);
 
-      // Define the first gradient
       const grad1 = svg.append('defs')
         .append('linearGradient')
         .attr('id', 'grad1')
@@ -69,7 +70,6 @@
           return 100 * (i / (colors1.length - 1)) + '%';
         });
 
-      // Define the second gradient
       const grad2 = svg.append('defs')
         .append('linearGradient')
         .attr('id', 'grad2')
@@ -87,35 +87,32 @@
           return 100 * (i / (colors2.length - 1)) + '%';
         });
 
-      // Add the first half of the bar
       const bar1 = svg.append("g");
       bar1.append("rect")
-        .attr('x', 0)
+        .attr('x', 20)
         .attr('y', 0)
         .attr('width', halfWidth)
         .attr('height', barHeight)
         .attr('fill', 'url(#grad1)');
 
-      // Add the second half of the bar
       const bar2 = svg.append("g");
       bar2.append("rect")
-        .attr('x', halfWidth)
+        .attr('x', halfWidth + 20)
         .attr('y', 0)
         .attr('width', halfWidth)
         .attr('height', barHeight)
         .attr('fill', 'url(#grad2)');
 
-      // Add legends (adjust based on actual text needed)
       bar1.append("text")
         .attr('x', 0)
         .attr('y', barHeight * 2)
-        .text($.i18n('study-idv-col-results-graphic-legend-1')); // replace with your localization method
+        .text($.i18n('study-idv-col-results-graphic-legend-1'));
 
       bar2.append("text")
         .attr('x', width)
         .attr('y', barHeight * 2)
         .attr('text-anchor', 'end')
-        .text($.i18n('study-idv-col-results-graphic-legend-2')); // replace with your localization method
+        .text($.i18n('study-idv-col-results-graphic-legend-2'));
 
       bar1.attr('transform', `translate(0, ${height / 2 - (barHeight / 2)})`);
       bar2.attr('transform', `translate(0, ${height / 2 - (barHeight / 2)})`);
